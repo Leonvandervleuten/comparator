9e33d718319d4a36aac1b9607911dd4239c8fad2
Leon van der Vleuten <Leonvandervleuten@gmail.com>
2022-05-25 16:29:53 +0200

    made postgresql because of value too long

--- src/main/java/com/digi/comparator/domain/SearchElements.java
+++ src/main/java/com/digi/comparator/domain/SearchElements.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,6 +21,7 @@ public class SearchElements {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(columnDefinition="TEXT")
   private String element;
 
   @ManyToOne
