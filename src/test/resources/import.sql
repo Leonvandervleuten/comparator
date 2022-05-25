7711d0567074c5c2a7b006fd59a900946e1200d3
Leon van der Vleuten <Leonvandervleuten@gmail.com>
2022-05-25 16:54:55 +0200

    Start with making tests

---
+++ src/test/resources/import.sql
@@ -0,0 +1,23 @@
+-- CsvFile
+insert into csv_file(id, file_name) values (1, filenaam1.csv)
+insert into csv_file(id, file_name) values (2, filenaam2.csv)
+insert into csv_file(id, file_name) values (3, filenaam3.csv)
+
+-- searchElements
+insert into search_elements(id, element, csv_file_id) values(1, 0612345678, 1)
+insert into search_elements(id, element, csv_file_id) values(2, 061234567823432432, 1)
+insert into search_elements(id, element, csv_file_id) values(3, +_)(*^%#@@%&*((*&%$#{}{:>>}), 1)
+insert into search_elements(id, element, csv_file_id) values(4, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum nunc quis dolor imperdiet luctus. Suspendisse vehicula, turpis eget rutrum sollicitudin, erat nibh molestie mi, in pretium nisl felis ut justo. Proin maximus ullamcorper tempus turpis., 1)
+insert into search_elements(id, element, csv_file_id) values(5, 12344, 1)
+
+insert into search_elements(id, element, csv_file_id) values(6, 0612345678, 2)
+insert into search_elements(id, element, csv_file_id) values(7, 061234567823432432, 2)
+insert into search_elements(id, element, csv_file_id) values(8, +_)(*^%#@@%&*((*&%$#{}{:>>}), 2)
+insert into search_elements(id, element, csv_file_id) values(9, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum nunc quis dolor imperdiet luctus. Suspendisse vehicula, turpis eget rutrum sollicitudin, erat nibh molestie mi, in pretium nisl felis ut justo. Proin maximus ullamcorper tempus turpis., 1)
+insert into search_elements(id, element, csv_file_id) values(10, 12344, 2)
+
+insert into search_elements(id, element, csv_file_id) values(6, 0612345678, 3)
+insert into search_elements(id, element, csv_file_id) values(7, 061234567823432432, 3)
+insert into search_elements(id, element, csv_file_id) values(8, +_)(*^%#@@%&*((*&%$#{}{:>>}), 3)
+insert into search_elements(id, element, csv_file_id) values(9, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum nunc quis dolor imperdiet luctus. Suspendisse vehicula, turpis eget rutrum sollicitudin, erat nibh molestie mi, in pretium nisl felis ut justo. Proin maximus ullamcorper tempus turpis., 1)
+insert into search_elements(id, element, csv_file_id) values(10, 12344, 3)
