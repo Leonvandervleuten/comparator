91965b60c25933241679ecaaf40068c597c3a065
Leon van der Vleuten <Leonvandervleuten@gmail.com>
2022-06-01 13:18:13 +0200

    Comparing lists and adding matches to hashset. Added DB in docker

--- src/main/java/com/digi/comparator/domain/SearchElements.java
+++ src/main/java/com/digi/comparator/domain/SearchElements.java
@@ -3,6 +3,7 @@ package com.digi.comparator.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,6 +15,7 @@ import javax.persistence.ManyToOne;
 @Entity
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 public class SearchElements {
 
--- src/main/java/com/digi/comparator/service/SearchElementsService.java
+++ src/main/java/com/digi/comparator/service/SearchElementsService.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -22,10 +23,13 @@ public class SearchElementsService {
   @Autowired
   private CsvFileRepository csvFileRepository;
 
+  private HashSet<SearchElements> matchList = new HashSet<>();
+
   private List<String> fromCsvToList(MultipartFile csv) throws IOException {
     byte[] bytes = csv.getBytes();
     String csvString = new String(bytes);
-    return new ArrayList<>(Arrays.asList(csvString.split(";")));
+    String adjustedCsvString = csvString.replaceAll("\r\n", "");
+    return new ArrayList<>(Arrays.asList(adjustedCsvString.split(";")));
   }
 
   public void addSearchElementsToH2DB(MultipartFile csv, Long csvId) throws IOException {
@@ -35,6 +39,7 @@ public class SearchElementsService {
 
     for (String element : elementList) {
       if (!element.isEmpty()) {
+        element.replace("", "inkomend");
         SearchElements searchElements = new SearchElements();
         searchElements.setElement(element);
         searchElements.setCsvFile(csvFile);
@@ -48,16 +53,37 @@ public class SearchElementsService {
   }
 
   public void compareElementsFromDB() {
-    int csvCount = csvFileRepository.findAll().size();
+    int totalCsvFiles = csvFileRepository.findAll().size();
+    for (int i = 1; i <= totalCsvFiles; i++) {
+      List<SearchElements> searchElementsHead = getSearchElementsByCSVFileId((long) i);
 
-    for (int i = 1; i <= csvCount; i++) {
-      List<SearchElements> searchElementsByCSVFileId = getSearchElementsByCSVFileId((long) i);
+      for (int y = 1; y <= totalCsvFiles; y++) {
+        if (y != i) {
+          List<SearchElements> searchElementsSlave = getSearchElementsByCSVFileId((long) y);
 
+          for (SearchElements headElement : searchElementsHead) {
+            for (SearchElements slaveElement : searchElementsSlave) {
+              if (headElement.getElement().contains(slaveElement.getElement())) {
+                matchMaker(headElement);
+              }
+            }
+          }
+        }
+      }
+    }
+    for (SearchElements element : matchList) {
+      System.out.println(element.getElement());
+      System.out.println(element.getCsvFile().getFileName());
     }
   }
 
   private List<SearchElements> getSearchElementsByCSVFileId(Long id) {
     return searchElementsRepository.getSearchElementsByCsvFile_Id(id);
+  }
 
+  private void matchMaker(SearchElements element) {
+    if (!(element.getElement().length() < 4 || element.getElement().length() > 25)) {
+      matchList.add(element);
+    }
   }
 }
